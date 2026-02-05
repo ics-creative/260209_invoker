c33f868f22ff48b4538bc72923af877814719c31
ics-nishihara <nishihara@ics-web.jp>
2026-02-05 11:45:10 +0900

    画像パスの修正

--- src/cart/products.ts
+++ src/cart/products.ts
@@ -1,9 +1,13 @@
+import product1 from "/product1.jpg";
+import product2 from "/product2.jpg";
+import product3 from "/product3.jpg";
+
 export interface Product {
   productId: string;
   productName: string;
   price: number;
   description: string;
-  imageUrl?: string;
+  imageUrl: string;
 }
 
 export const PRODUCTS: Product[] = [
@@ -12,20 +16,20 @@ export const PRODUCTS: Product[] = [
     productName: "ワイヤレスヘッドホン",
     price: 8980,
     description: "UnsplashのYearOneが撮影した写真 ",
-    imageUrl: "/product1.jpg",
+    imageUrl: product1,
   },
   {
     productId: "product-002",
     productName: "スマートウォッチ",
     price: 15800,
     description: "UnsplashのRachit Tankが撮影した写真",
-    imageUrl: "/product2.jpg",
+    imageUrl: product2,
   },
   {
     productId: "product-003",
     productName: "ノートPC",
     description: "UnsplashのKari Sheaが撮影した写真",
     price: 89800,
-    imageUrl: "/product3.jpg",
+    imageUrl: product3,
   },
 ];
